37f02b443ef2426be91d12dcea8bcb35baf4a96a
Dralagar George <dralagar@gmail.com>
2025-02-13 11:27:47 +0300

    update

--- app/components/StripeDonation.tsx
+++ app/components/StripeDonation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
