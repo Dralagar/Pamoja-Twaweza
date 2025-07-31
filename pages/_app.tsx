b75bba43a796c59595f11f8d046d63db657f0f78
Dralagar George <dralagar@gmail.com>
2025-07-31 14:28:04 +0300

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'; // Make sure this file exists!
+import './styles/globals.css'; // Make sure this file exists!
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
