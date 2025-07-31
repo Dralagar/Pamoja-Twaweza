aa9773de5e5d8ae8a20bba0e417f46bb0e3010d7
Dralagar George <dralagar@gmail.com>
2025-07-31 14:17:21 +0300

    updated globals.css and updated _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import '../styles/globals.css'; // Make sure this file exists!
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp; 
+export default MyApp;
